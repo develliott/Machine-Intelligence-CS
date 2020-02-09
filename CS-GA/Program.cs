0fa62b32e8483ab15aa574ce81fa08419bc7055d
Elliott Record <elliottrecord@protonmail.com>
2020-02-09 13:52:08 +0000

    Parsed CSV into 2d array in StudentPreferenceData object.

--- CS-GA/Program.cs
+++ CS-GA/Program.cs
@@ -11,29 +11,70 @@ namespace CS_GA
             Console.WriteLine("Hello World!");
             bool ignoreHeader = true;
 
+            // TODO: Dynamically set data array length
+            StudentPreferenceData studentPreferenceData = new StudentPreferenceData(new int[7,14]);
+
             using (var reader = new StreamReader("F:\\repos\\CS-Machine-Intelligence-Algorithms\\Data\\StudentData.csv"))
             {
+                if (ignoreHeader)
+                    reader.ReadLine();
+
+                int currentRowIndex = 0;
                 while (!reader.EndOfStream)
                 {
-                    if (ignoreHeader)
-                        reader.ReadLine();
-
+                    // New Line of Student Data
                     var basicFileLine = reader.ReadLine();
 
                     if (basicFileLine != null)
                     {
-                        var itemsInFileLine = basicFileLine.Split(",").ToList();
+                        // Split the line by commands to retrieve a list of strings 
+                        var itemsInFileLine = basicFileLine.Split(",");
 
-                        itemsInFileLine.ForEach(element => Console.WriteLine(element));
+                        // TODO: Change column index, and deal with the Student Name
+                        for (int currentColumnIndex = 1; currentColumnIndex < itemsInFileLine.Length; currentColumnIndex++)
+                        {
+                            string preferenceAsLowerCase = itemsInFileLine[currentColumnIndex].ToLower();
+                            int preferenceScore = ConvertStringDataToPreferenceScore(preferenceAsLowerCase);
+
+                            //TODO: Change 'currentColumnIndex - 1'. It was done to get around the student name column causing an index out of bounds exception
+                            studentPreferenceData.SetStudentPreference(currentRowIndex, currentColumnIndex - 1, preferenceScore);
+                        }
                     }
                     else
                     {
                         throw new NullReferenceException();
                     }
+
+                    currentRowIndex++;
                 }
             }
 
+            var studentData = studentPreferenceData.GetStudentData(6).ToList();
+            studentData.ForEach(data => Console.WriteLine(data));
+            
+
             Console.ReadLine();
+        }
+
+        static int ConvertStringDataToPreferenceScore(string data)
+        {
+            int preferenceScore;
+            switch (data)
+            {
+                case "no":
+                    preferenceScore = -10;
+                    break;
+                case "ok":
+                    preferenceScore = 1;
+                    break;
+                case "pref":
+                    preferenceScore = 10;
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            return preferenceScore;
         }
     }
 }
---
+++ CS-GA/StudentPreferenceData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS_GA
+{
+    public class StudentPreferenceData
+    {
+        // Rows then Columns for index size.
+        private readonly int[,] _studentPreference;
+
+        public StudentPreferenceData(int[,] studentPreference)
+        {
+            _studentPreference = studentPreference;
+        }
+
+        public void SetStudentPreference(int studentIndex, int timeslotIndex, int preferenceScore )
+        {
+            _studentPreference[studentIndex, timeslotIndex] = preferenceScore;
+        }
+
+        public int GetStudentPreference(int studentIndex, int timeslotIndex)
+        {
+            return _studentPreference[studentIndex, timeslotIndex];
+        }
+
+        public int[] GetStudentData(int studentIndex)
+        {
+            return Enumerable.Range(0, _studentPreference.GetLength(1))
+                .Select(x => _studentPreference[studentIndex, x])
+                .ToArray();
+        }
+    }
+}
