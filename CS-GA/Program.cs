ce3e7a7a1d3f758a40f197f8b22d6ba8149e9acc
Elliott Record <elliottrecord@protonmail.com>
2020-02-09 13:15:31 +0000

    Implemented parsing the CSV file.

--- CS-GA/Program.cs
+++ CS-GA/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace CS_GA
 {
@@ -7,6 +9,31 @@ namespace CS_GA
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            bool ignoreHeader = true;
+
+            using (var reader = new StreamReader("F:\\repos\\CS-Machine-Intelligence-Algorithms\\Data\\StudentData.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    if (ignoreHeader)
+                        reader.ReadLine();
+
+                    var basicFileLine = reader.ReadLine();
+
+                    if (basicFileLine != null)
+                    {
+                        var itemsInFileLine = basicFileLine.Split(",").ToList();
+
+                        itemsInFileLine.ForEach(element => Console.WriteLine(element));
+                    }
+                    else
+                    {
+                        throw new NullReferenceException();
+                    }
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
