a0a7e1ae64998c1a275b218d41cccae339f5ea82
Elliott Record <elliottrecord@protonmail.com>
2020-02-10 12:15:42 +0000

    Moved DI into Ninject Module.

--- CS-GA/Ninject/Bindings.cs
+++ CS-GA/Ninject/Bindings.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CS_GA.Data;
+using CS_GA.Genetic_Algorithm;
+using Ninject;
 using Ninject.Modules;
+using Ninject.Parameters;
 
 namespace CS_GA.Ninject
 {
     public class Bindings : NinjectModule
     {
+        private const string _filePath = "F:\\repos\\CS-Machine-Intelligence-Algorithms\\Data\\StudentData.csv";
+
         public override void Load()
         {
-            
+            var csvHelper = Kernel.Get<CsvHelper<string>>(new ConstructorArgument("filePath", _filePath),
+                new ConstructorArgument("delimiter", ","), new ConstructorArgument("ignoreFirstRow", true),
+                new ConstructorArgument("ignoreFirstColumn", true));
+
+
+            var studentPreferenceData = Kernel.Get<StudentPreferenceData<int>>(
+                new ConstructorArgument("maxRowIndex", csvHelper.MaxRowIndex),
+                new ConstructorArgument("maxColumnIndex", csvHelper.MaxColumnIndex), new ConstructorArgument("csvFileData", csvHelper.GetCsvFileData()));
+
+            FitnessCalculator.SetStudentPreferenceData(studentPreferenceData);
+
+            Kernel.Bind<IEvolutionService>().To<EvolutionService>();
         }
     }
 }
--- CS-GA/Program.cs
+++ CS-GA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CS_GA.Data;
 using CS_GA.Genetic_Algorithm;
 using Ninject;
 using Ninject.Parameters;
@@ -7,25 +8,12 @@ namespace CS_GA
 {
     internal class Program
     {
-        private const string _filePath = "F:\\repos\\CS-Machine-Intelligence-Algorithms\\Data\\StudentData.csv";
 
         private static void Main(string[] args)
         {
             IKernel kernel = new StandardKernel(new Ninject.Bindings());
 
-
-            var csvHelper = kernel.Get<CsvHelper<string>>(new ConstructorArgument("filePath", _filePath),
-                new ConstructorArgument("delimiter", ","), new ConstructorArgument("ignoreFirstRow", true),
-                new ConstructorArgument("ignoreFirstColumn", true));
-
-
-            var studentPreferenceData = kernel.Get<StudentPreferenceData<int>>(
-                new ConstructorArgument("maxRowIndex", csvHelper.MaxRowIndex),
-                new ConstructorArgument("maxColumnIndex", csvHelper.MaxColumnIndex), new ConstructorArgument("csvFileData", csvHelper.GetCsvFileData()));
-            
-            FitnessCalculator.SetStudentPreferenceData(studentPreferenceData);
-
-            var secondMain = new SecondMain();
+            var secondMain = kernel.Get<SecondMain>();
         }
 
        
