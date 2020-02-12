eac691900c24dbf64f6643b5c8b61050c8c9bc46
Elliott Record <elliottrecord@protonmail.com>
2020-02-12 02:26:41 +0000

    Implemented IMutationOperator as SwapMutation.

---
+++ CS-GA/Business/Operators/IMutationOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CS_GA.Business.Common;
+
+namespace CS_GA.Business.Operators
+{
+    interface IMutationOperator
+    {
+        IChromosome<int> Mutate(IChromosome<int> chromosome);
+    }
+}
---
+++ CS-GA/Business/Operators/SwapMutation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CS_GA.Business.Common;
+
+namespace CS_GA.Business.Operators
+{
+    public class SwapMutation : IMutationOperator
+    {
+        private Random _random = new Random();
+
+        public IChromosome<int> Mutate(IChromosome<int> chromosome)
+        {
+            // TODO: Implement infinity searching with boundary constraints
+
+            // === Find 2 indices at random.
+            var swapIndex1 = _random.Next(chromosome.Size);
+            var swapIndex2 = _random.Next(chromosome.Size);
+
+            // Ensure indices are unique.
+            while (swapIndex1 == swapIndex2)
+            {
+                swapIndex2 = _random.Next(chromosome.Size);
+            }
+
+            // Store the original values.
+            var swapIndex1OriginalValue = chromosome.GetGeneValue(swapIndex1);
+            var swapIndex2OriginalValue = chromosome.GetGeneValue(swapIndex2);
+
+            // Swap the index values.
+            chromosome.SetGeneValue(swapIndex1, swapIndex2OriginalValue);
+            chromosome.SetGeneValue(swapIndex2, swapIndex1OriginalValue);
+
+            return chromosome;
+        }
+    }
+}
