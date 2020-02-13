824751a01a3dbf105eb13558496ee4c11fb1a774
Elliott Record <elliottrecord@protonmail.com>
2020-02-13 02:42:29 +0000

    Removed ChromosomeConstaint because it wasn't used.

--- CS-GA/Ninject/Bindings.cs
+++ CS-GA/Ninject/Bindings.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Linq;
-using CS_GA.Business.Common;
-using CS_GA.Business.Common.Data_Structure;
+﻿using CS_GA.Business.Common.Data_Structure;
 using CS_GA.Business.Common.Factories;
 using CS_GA.Business.Common.Operators;
 using CS_GA.Business.Common.Strategies;
 using CS_GA.Business.Data_Structure;
-using CS_GA.Business.Data_Structure.Helpers;
 using CS_GA.Business.Operators;
 using CS_GA.Business.Strategies;
 using CS_GA.DAL;
@@ -37,10 +33,6 @@ namespace CS_GA.Ninject
                 .WithConstructorArgument("maxColumnIndex", csvHelper.MaxColumnIndex)
                 .WithConstructorArgument("csvFileData", csvHelper.GetCsvFileData());
             Kernel.Bind<IEnvironmentService>().To<EnvironmentService>().InSingletonScope();
-
-            // Helper
-            Kernel.Bind<IChromosomeConstraint>().To<ChromosomeConstraint>().InSingletonScope()
-                .WithConstructorArgument("maxGeneValue", csvHelper.MaxRowIndex);
 
             // Define Strategies and Operators
             Kernel.Bind<ISelectionStrategy>().To<TournamentSelection>().InSingletonScope();
